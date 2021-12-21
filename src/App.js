7301559e4a48608113fc8147dabbb17847e6db00
Sreejan <sreejan147@gmail.com>
2021-12-21 13:27:24 +0530

    feat: add navbar, hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import programming from "./assets/programming.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div className="app-content">
+      <nav className="navbar">
+        <h1 className="logo" onClick={() => window.location.reload()}>
+          Sreejan
+        </h1>
+        <div className="nav-links">
+          <a href="#projects">Projects</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="#contact">Contact</a>
+        </div>
+      </nav>
+      <section className="hero-section">
+        <div className="hero-left">
+          <h1>Hey, I'm Sreejan! 👋</h1>
+          <br />
+          <h3>
+            I am a software developer from India. I am enthusiastic about
+            building impactful products. I am into full stack web development
+            with specialization in frontend.
+          </h3>
+          <br />
+          <br />
+          <br />
+          <div className="hero-links">
+            <a
+              href="https://github.com/Sreejan-22"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+        <div className="hero-right">
+          <img src={programming} alt="" />
+        </div>
+      </section>
     </div>
   );
 }
