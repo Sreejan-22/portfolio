6019fb0341ad58a557ddabed6c9f241baaa4f628
Sreejan <sreejan147@gmail.com>
2021-12-21 22:25:48 +0530

    add skills section, improve styles of ProjectCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ProjectCard from "./components/ProjectCard/ProjectCard";
 import projectData from "./data/projectData";
+import skills from "./data/skills";
 import programming from "./assets/programming.svg";
 import "./App.css";
 
@@ -12,6 +13,8 @@ function App() {
         </h1>
         <div className="nav-links">
           <a href="#projects">Projects</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="#skills">Skills</a>
           &nbsp;&nbsp;&nbsp;&nbsp;
           <a href="#contact">Contact</a>
         </div>
@@ -44,14 +47,29 @@ function App() {
       <section className="project-section" id="projects">
         <h1>Projects</h1>
         <div className="project-container">
-          {projectData.map((_, index) => (
+          {projectData.map((_) => (
             <ProjectCard
               name={_.name}
               description={_.description}
               live={_.live}
               source={_.source}
-              key={index}
+              key={_.id}
             />
+          ))}
+        </div>
+        <br />
+        <br />
+        <br />
+      </section>
+      <section className="skills-section">
+        <h1>Skills</h1>
+        <div className="skills" id="skills">
+          {skills.map((item) => (
+            <div className="skill" key={item.id}>
+              <img src={item.url} alt="" />
+              <br />
+              <p>{item.name}</p>
+            </div>
           ))}
         </div>
         <br />
---
+++ src/data/skills.js
@@ -0,0 +1,44 @@
+const skills = [
+  {
+    id: 1,
+    name: "Javascript",
+    url: "/images/js.svg",
+  },
+  {
+    id: 2,
+    name: "React",
+    url: "/images/react.svg",
+  },
+  {
+    id: 3,
+    name: "HTML",
+    url: "/images/html.svg",
+  },
+  {
+    id: 4,
+    name: "CSS",
+    url: "/images/css.svg",
+  },
+  {
+    id: 5,
+    name: "Node.js",
+    url: "/images/node.svg",
+  },
+  {
+    id: 6,
+    name: "Redux",
+    url: "/images/redux.svg",
+  },
+  {
+    id: 7,
+    name: "MaterialUI",
+    url: "/images/materialui.svg",
+  },
+  {
+    id: 8,
+    name: "ChakraUI",
+    url: "/images/chakra.svg",
+  },
+];
+
+export default skills;
